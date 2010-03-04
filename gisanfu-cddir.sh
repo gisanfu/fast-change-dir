d6a7c8f1b37f3f84f58398c8a159c396b4904131
gisanfu <gisanfu@gisanfu-desktop.(none)>
2010-03-04 17:20:36 +0800

    add file by newfunction

---
+++ gisanfu-cddir.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source 'gisanfu-function.sh'
+
+# fix space to effect array result
+IFS=$'\012'
+
+nextRelativeItem=$1
+secondCondition=$2
+
+#searchCondition='ls -AF | grep "/$" | grep -ir ^$nextRelativeItem'
+
+itemList=(`ls -AF | grep "/$" | grep -ir ^$nextRelativeItem`)
+
+if [ "$secondCondition" != "" ]; then
+	itemList2=(`ls -AF | grep "/$" | grep -ir ^$nextRelativeItem | grep -ir $secondCondition`)
+fi
+
+. gisanfu-relative.sh
+
+if [ "$relativeitem" != "" ]; then
+	cd $relativeitem
+	# check file count and ls action
+	func_checkfilecount
+fi
---
+++ gisanfu-editfile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source 'gisanfu-function.sh'
+
+# fix space to effect array result
+IFS=$'\012'
+
+nextRelativeItem=$1
+secondCondition=$2
+
+itemList=(`ls -AF --file-type | grep -v "/$" | grep -ir ^$nextRelativeItem`)
+
+if [ "$secondCondition" != "" ]; then
+	itemList2=(`ls -AF --file-type | grep -v "/$" | grep -ir ^$nextRelativeItem | grep -ir $secondCondition`)
+fi
+
+. gisanfu-relative.sh
+
+vim $relativeitem
+
+relativeitem=''
+itemList=''
