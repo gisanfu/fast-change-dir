9afaec53a9acd7cb1c50cf11739bbfdbd0daad4d
gisanfu <gisanfu@gisanfu-umpc.(none)>
2010-04-04 11:31:04 +0800

    fix bug

--- gisanfu-dirpoint.sh
+++ gisanfu-dirpoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source 'gisanfu-function.sh'
+
 dirpoint=$1
 
 if [ "$dirpoint" != "" ]; then
@@ -7,6 +9,8 @@ if [ "$dirpoint" != "" ]; then
 		result=`grep $dirpoint ~/gisanfu-dirpoint-$groupname.txt | cut -d, -f2`
 		if [ "$result" != "" ]; then
 			cd $result
+			# check file count and ls action
+			func_checkfilecount
 		else
 			echo '[ERROR] dirpoint is not exist!!'
 		fi
