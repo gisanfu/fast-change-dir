f2a21de92298feb951dc64d89e834906adf9cd5b
gisanfu <gisanfu@gisanfu-umpc.(none)>
2010-04-05 17:16:51 +0800

    fix bug

--- gisanfu-backdir.sh
+++ gisanfu-backdir.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
-#source 'gisanfu-function.sh'
+source 'gisanfu-function.sh'
 
-nextRelativeChdir=$1
+# fix space to effect array result
+IFS=$'\012'
+
+nextRelativeItem=$1
 secondCondition=$2
 
-cd .. && . gisanfu-cddir.sh $nextRelativeChdir $secondCondition
+#searchCondition='ls -AF | grep "/$" | grep -ir ^$nextRelativeItem'
 
-if [ "$relativeitem" == "" ]; then
-	cd -
+itemList=(`ls -AF | grep "/$" | grep -ir ^$nextRelativeItem`)
+
+if [ "$secondCondition" != "" ]; then
+	itemList2=(`ls -AF | grep "/$" | grep -ir ^$nextRelativeItem | grep -ir $secondCondition`)
 fi
+
+. gisanfu-relative.sh
+
+if [ "$relativeitem" != "" ]; then
+	cd ../$relativeitem
+	# check file count and ls action
+	func_checkfilecount
+fi
+
+relativeitem=''
+itemList=''
