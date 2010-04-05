f26b6538e01742c5cdcd82e001b448a0ebade2ff
gisanfu <gisanfu@gisanfu-laptop.(none)>
2010-04-05 23:11:33 +0800

    add groupname file and function

---
+++ gisanfu-groupname.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+action=$1
+groupname=$2
+
+if [ "$action" == "select" ]; then
+	count=`grep -ir $groupname ~/gisanfu-groupname.txt | wc -l`
+	if [ "$count" == "0" ]; then
+		echo "[ERROR] groupname is not exist by $groupname"
+	else
+		export groupname=$groupname
+		echo '[OK] export groupname success'
+	fi
+elif [ "$action" == "append" ]; then
+	count=`grep -ir $groupname ~/gisanfu-groupname.txt | wc -l`
+	if [ "$count" == "0" ]; then
+		echo $groupname >> ~/gisanfu-groupname.txt
+		echo '[OK] append groupname success'
+	else
+		echo "[ERROR] groupname is exist by $groupname"
+	fi
+elif [ "$action" == "edit" ]; then
+	vim ~/gisanfu-groupname.txt
+else
+	echo '[ERROR] no support action'
+fi
