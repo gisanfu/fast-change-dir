79c98278df445e424f890a62f4a7b84c606a065f
gisanfu <gisanfu@gisanfu-desktop.(none)>
2010-09-19 14:20:42 +0800

    fix symbolic problem

--- gisanfu-pos-editfile.sh
+++ gisanfu-pos-editfile.sh
@@ -8,7 +8,7 @@ IFS=$'\012'
 filePosition=$1
 
 if [ "$filePosition" != "" ]; then
-	fileList=(`ls -AF --file-type|grep -v "/$"`)
+	fileList=(`ls -AFL --file-type|grep -v "/$"`)
 	if [ "${#fileList[@]}" -lt 1 ]; then
 		func_statusbar 'THERE-IS-NO-FILE'
 	else	
