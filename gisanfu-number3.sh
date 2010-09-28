481013e5cb9357213fdc5b99e36f062c254a1a64
gisanfu <gisanfu@gisanfu-desktop.(none)>
2010-09-28 11:53:50 +0800

    prototype ok

--- gisanfu-number3.sh
+++ gisanfu-number3.sh
@@ -24,7 +24,12 @@ func_relative()
 	# dir or file
 	filetype=$3
 
-	#echo "bbb $nextRelativeItem"
+	declare -a itemList
+	declare -a itemList2
+	#declare -a relativeitem 
+
+	#echo "one $nextRelativeItem"
+	#echo "second $secondCondition"
 
 	if [ "$filetype" == "dir" ]; then
 		filetype_ls_arg=''
@@ -34,19 +39,24 @@ func_relative()
 		filetype_grep_arg='-v'
 	fi
 
-	itemList=(`ls -AF $filetype_ls_arg | grep $filetype_grep_arg "/$" | grep -ir ^$nextRelativeItem`)
+	IFS=" "
+	itemList=(`ls -AF -I .svn -I .git $filetype_ls_arg | grep $filetype_grep_arg "/$" | grep -ir ^$nextRelativeItem | tr -s "\n" " "` )
+	#echo $itemList
+	#echo "itemList數量 ${#itemList[@]}"
 
 	# use (^) grep fast, if no match, then remove (^)
 	if [ "${#itemList[@]}" -lt "1" ]; then
-		itemList=(`ls -AF $file_ls_arg | grep $filetype_grep_arg "/$" | grep -ir $nextRelativeItem`)
-		if [[ "${#itemList[@]}" -gt "1" && "$secondCondition" != "" ]]; then
-			itemList2=(`ls -AF $file_ls_arg | grep $filetype_grep_arg "/$" | grep -ir $nextRelativeItem | grep -ir $secondCondition`)
+		itemList=(`ls -AF -I .svn -I .git $file_ls_arg | grep $filetype_grep_arg "/$" | grep -ir $nextRelativeItem`)
+		if [[ "${#itemList[@]}" -gt "1" && "$secondCondition" != '' ]]; then
+			itemList2=(`ls -AF -I .svn -I .git $file_ls_arg | grep $filetype_grep_arg "/$" | grep -ir $nextRelativeItem | grep -ir $secondCondition`)
 		fi
-	elif [ "${#itemList[@]}" -gt "1" ]; then
-		if [ "$secondCondition" != "" ]; then
-			itemList2=(`ls -AF $file_ls_arg | grep $filetype_grep_arg "/$" | grep -ir ^$nextRelativeItem | grep -ir $secondCondition`)
-		fi
+	elif [[ "${#itemList[@]}" -gt "1" && "$secondCondition" != '' ]]; then
+		#echo 'aaaaaaaaaaaaaaaaaaaaaaa'
+		itemList2=(`ls -AF -I .svn -I .git $file_ls_arg | grep $filetype_grep_arg "/$" | grep -ir ^$nextRelativeItem | grep -ir $secondCondition`)
 	fi
+	IFS=""
+
+	#echo $itemList2
 
 	# if empty of variable, then go back directory
 	if [ "$nextRelativeItem" == "" ]; then
@@ -58,13 +68,13 @@ func_relative()
 			#func_statusbar 'USE-ITEM'
 		elif [ "${#itemList[@]}" -gt "1" ]; then
 
-			if [ "$secondCondition" == "" ]; then
+			if [ "$secondCondition" == '' ]; then
 				# if have duplicate dirname then CHDIR
 				Success="0"
 				for dirDuplicatelist in ${itemList[@]}
 				do
 					# to match file or dir rule
-					if [ "$dirDuplicatelist" == "$nextRelativeItem/" ] || [ "$dirDuplicatelist" == "$nextRelativeItem" ]; then
+					if [[ "$dirDuplicatelist" == "$nextRelativeItem/" || "$dirDuplicatelist" == "$nextRelativeItem" ]]; then
 						relativeitem=$nextRelativeItem
 
 						#func_statusbar 'USE-LUCK-ITEM'
@@ -85,33 +95,33 @@ func_relative()
 			fi
 		
 			# if no duplicate dirname then print them
-			if [ $Success == "0" ]; then
-				dialogitems=''
-				for echothem in ${itemList[@]}
-				do
-					dialogitems="$dialogitems \"$echothem\""
-				done
+			#if [ $Success == "0" ]; then
+			#	#dialogitems=''
+			#	#for echothem in ${itemList[@]}
+			#	#do
+			#	#	dialogitems="$dialogitems \"$echothem\""
+			#	#done
 
-				relativeitem=$dialogitems
+			#	relativeitem=$itemList
 
-				#cmd=$( func_dialog_menu 'Please Select Item' 70 "$dialogitems" "$tmpfile" )
+			#	#cmd=$( func_dialog_menu 'Please Select Item' 70 "$dialogitems" "$tmpfile" )
 
-				#eval $cmd
-				#result=`cat $tmpfile`
+			#	#eval $cmd
+			#	#result=`cat $tmpfile`
 
-				#if [ -f "$tmpfile" ]; then
-				#	rm -rf $tmpfile
-				#fi
-				#if [ "$result" == "" ]; then
-				#	func_statusbar 'PLEASE-SELECT-ONE-ITEM'
-				#	for echothem in ${itemList[@]}
-				#	do
-				#		echo $echothem
-				#	done
-				#else
-				#	relativeitem=$result
-				#fi
-			fi
+			#	#if [ -f "$tmpfile" ]; then
+			#	#	rm -rf $tmpfile
+			#	#fi
+			#	#if [ "$result" == "" ]; then
+			#	#	func_statusbar 'PLEASE-SELECT-ONE-ITEM'
+			#	#	for echothem in ${itemList[@]}
+			#	#	do
+			#	#		echo $echothem
+			#	#	done
+			#	#else
+			#	#	relativeitem=$result
+			#	#fi
+			#fi
 		else
 			relativeitem=''
 			#func_statusbar 'NOT-FOUND-OR-NOT-EXIST'
@@ -121,13 +131,22 @@ func_relative()
 	echo $relativeitem
 }
 
+#IFS=" "
+
 unset condition
+unset cmd1
+unset cmd2
 unset item_file_array
 unset item_dir_array
 
 while [ 1 ];
 do
 	clear
+	echo "cmd1 $cmd1"
+	echo "cmd2 $cmd2"
+	echo $relativeitem
+	echo $item_dir
+	echo $item_file
 	ls -AF -I .svn -I .git --color=auto
 
 	if [ "$condition" != '' ]; then
@@ -137,25 +156,44 @@ do
 
 	echo '================================================='
 
-	# 顯示重覆檔案
-	#if [ "${#item_file_array[@]}" -gt 1 ]; then
-	#	echo "重覆的檔案數量: ${#item_file_array[@]}"
-	#	for bbb in ${item_file_array[@]}
-	#	do
-	#		echo $bbb
-	#	done
-	#	continue
-	#elif [ "${#item_file_array[@]}" == 1 ]; then 
-	#	if [ "$groupname" != '' ]; then
-	#		vf ${item_file_array[0]}
+	#if [[ "${#item_file_array[@]}" == 1 && "${#item_dir_array[@]}" == 1 ]]; then 
+	#	echo '資料夾和檔案，都同時擁有1筆符合條件的，你要哪一個呢？'
+	#	read -s -n 1 inputselectdirorfile
+	#	if [ "$inputselectdirorfile" == 'd' ]; then
+	#		d ${item_dir_array[0]}
 	#	else
-	#		vim ${item_file_array[0]}
+	#		if [ "$groupname" != '' ]; then
+	#			vf ${item_file_array[0]}
+	#		else
+	#			vim ${item_file_array[0]}
+	#		fi
 	#	fi
+
 	#	unset condition
 	#	unset item_file_array
 	#	unset item_dir_array
-	#	continue
 	#fi
+
+	# 顯示重覆檔案
+	if [ "${#item_file_array[@]}" -gt 1 ]; then
+		echo "重覆的檔案數量: ${#item_file_array[@]}"
+		for bbb in ${item_file_array[@]}
+		do
+			echo $bbb
+		done
+		#continue
+	elif [ "${#item_file_array[@]}" == 1 ]; then 
+		echo "檔案有找到一筆哦: ${item_file_array[0]}"
+		#if [ "$groupname" != '' ]; then
+		#	vf ${item_file_array[0]}
+		#else
+		#	vim ${item_file_array[0]}
+		#fi
+		#unset condition
+		#unset item_file_array
+		#unset item_dir_array
+		#continue
+	fi
 
 	# 顯示重覆資料夾
 	if [ "${#item_dir_array[@]}" -gt 1 ]; then
@@ -164,12 +202,13 @@ do
 		do
 			echo $bbb
 		done
-		exit
-	elif [ "${#item_dir_array[@]}" == 1 ]; then 
-		d ${item_dir_array[0]}
-		unset condition
-		unset item_file_array
-		unset item_dir_array
+		#exit
+	elif [ "${#item_dir_array[@]}" -eq 1 ]; then 
+		echo "資料夾有找到一筆哦: ${item_dir_array[0]}"
+		#d ${item_dir_array[0]}
+		#unset condition
+		#unset item_file_array
+		#unset item_dir_array
 	fi
 
 	read -s -n 1 inputvar
@@ -185,29 +224,50 @@ do
 		unset item_file_array
 		unset item_dir_array
 		continue
+	elif [[ "$inputvar" == 'D' && "${#item_dir_array[@]}" == 1 ]]; then
+		run="d ${item_dir_array[0]}"
+		eval $run
+		unset condition
+		unset item_file_array
+		unset item_dir_array
+		continue
+	elif [[ "$inputvar" == 'F' && "${#item_file_array[@]}" == 1 ]]; then
+		if [ "$groupname" != '' ]; then
+			run="vf ${item_file_array[0]}"
+		else
+			run="vim ${item_file_array[0]}"
+		fi
+		eval $run
+		unset condition
+		unset item_file_array
+		unset item_dir_array
+		continue
 	fi
 
 	condition="$condition$inputvar"
 
 	if [[ "$condition" =~ [[:alnum:]] ]]; then
+		IFS=" "
+		declare -a cmds
 		cmds=(`echo $condition`)
 		cmd1=${cmds[0]}
 		cmd2=${cmds[1]}
+		IFS=""
 
-		#echo $cmd1
 
 		item_dir=$( func_relative "$cmd1" "$cmd2" "dir" )
 		item_file=$( func_relative "$cmd1" "$cmd2" "file" )
 
 		item_dir_array=(`echo $item_dir`)
-		#item_file_array=(`echo $item_file`)
+		item_file_array=(`echo $item_file`)
 
-		echo $item_dir
+		#echo $item_dir
+		#echo $item_file
 
-		for aaab in ${item_dir_array[@]}
-		do
-			echo $aaab
-		done
-		sleep 20
+		#for aaab in ${item_dir_array[@]}
+		#do
+		#	echo $aaab
+		#done
+		#sleep 20
 	fi
 done
