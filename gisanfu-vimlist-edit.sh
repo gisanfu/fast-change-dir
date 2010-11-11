09c80d326f2bc9deb3d9e3bebbb13ce5afd6113f
gisanfu <gisanfu@gisanfu-laptop-r60.(none)>
2010-11-11 20:13:38 +0800

    fix vff, add other option

--- gisanfu-vimlist-edit.sh
+++ gisanfu-vimlist-edit.sh
@@ -2,6 +2,13 @@
 
 if [ "$groupname" != "" ]; then
 	vim ~/gisanfu-vimlist-$groupname.txt
+
+	# 詢問使用者，看要不要編輯list
+	echo '[WAIT] 預設是編輯暫存檔案群，我指的是vff [Y1,n0]'
+	read -n 1 inputchar
+	if [[ "$inputchar" == 'y' || "$inputchar" == '1' || "$inputchar" == '' ]]; then
+		vff
+	fi
 else
 	echo '[ERROR] groupname is empty'
 fi
