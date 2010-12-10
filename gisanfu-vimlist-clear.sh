30fc45618bb9d329ad2996e9313f563b438e35eb
Jerry <gisanfu@gmail.com>
2010-12-10 16:56:54 +0800

    修正送出要清空vim暫存檔的時候，沒有清空的問題

--- gisanfu-vimlist-clear.sh
+++ gisanfu-vimlist-clear.sh
@@ -3,15 +3,12 @@
 if [ "$groupname" != '' ]; then
 	echo '你確定要清空vim暫存群組檔嗎?[Y1, n0]'
 	read -s -n 1 inputvar
-	if [[ "$inputvar" == 'y' || "$inputvar" == "1" ]]; then
-		echo '' > ~/gisanfu-vimlist-$groupname.txt
-		echo '己清空'
-		sleep 1
-	elif [[ "$inputvar" == 'n' || "$inputvar" == "0" ]]; then
+	if [[ "$inputvar" == 'n' || "$inputvar" == "0" ]]; then
 		echo '己取消清空vim暫存群組檔'
 		sleep 1
 	else
-		echo '己取消清空vim暫存群組檔'
+		echo '' > ~/gisanfu-vimlist-$groupname.txt
+		echo '己清空'
 		sleep 1
 	fi
 fi
