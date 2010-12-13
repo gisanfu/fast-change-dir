547d43cc1fad7cb52ebe7c0e4d29709a447bebb7
Jerry <gisanfu@gmail.com>
2010-12-13 18:02:53 +0800

    fix bug

--- gisanfu-vimlist.sh
+++ gisanfu-vimlist.sh
@@ -6,18 +6,22 @@ program=$1
 
 if [ "$groupname" != "" ]; then
 	if [ "$program" == "" ]; then
-		program="vim -p ~/gisanfu-vimlist-$groupname.txt"
+		program2="vim -p ~/gisanfu-vimlist-$groupname.txt"
+	else
+		program2=$program
 	fi
 
 	cmdlist=`cat ~/gisanfu-vimlist-$groupname.txt | tr "\n" " "`
-	cmd="$program $cmdlist"
+	cmd="$program2 $cmdlist"
 
 	# 檢查一下數量，如果是一筆的話，那就自動跳到那一筆
 	# 這樣子只有一筆的時候，會比較方便
-	count=`cat ~/gisanfu-vimlist-$groupname.txt | wc -l`
-
-	if [ "$count" == '1' ]; then
-		cmd="$cmd +tabnext"
+	# 會這樣子寫，是因為不要去影響其它程式的相依性
+	if [ "$program" == "" ]; then
+		count=`cat ~/gisanfu-vimlist-$groupname.txt | wc -l`
+		if [ "$count" == '1' ]; then
+		  cmd="$cmd +tabnext"
+		fi
 	fi
 
 	eval $cmd
