31e74596d0f5f14124f33f14fbe9ea203cf87f73
Jerry <gisanfu@gmail.com>
2010-12-30 18:30:36 +0800

    暫時結束工作

--- gisanfu-svn-3.sh
+++ gisanfu-svn-3.sh
@@ -46,6 +46,10 @@ func_relative_by_svn_append()
 	# 顯示的方式，可能是untracked or tracked
 	modestatus=$4
 
+	# cache的檔名也是用變數控制
+	uncachefile=$5
+	cachefile=$6
+
 	declare -a itemList
 	declare -a itemListTmp
 	declare -a itemList2
@@ -72,6 +76,11 @@ func_relative_by_svn_append()
 		itemListTmp=(`svn status | grep -e '^?' -e '^!' | grep -ir $nextRelativeItem`)
 	elif [ "$modestatus" == 'commit' ]; then
 		itemListTmp=(`svn status | grep -e '^A' -e '^D' | grep -ir $nextRelativeItem`)
+	elif [ "$modestatus" == 'uncache' ]; then
+		cmd="cat $uncachefile | grep -ir $nextRelativeItem"
+		itemListTmp=(`eval $cmd`)
+	elif [ "$modestatus" == 'cache' ]; then
+		itemListTmp=(`cat $cachefile | grep -ir $nextRelativeItem`)
 	fi
 
 	for i in ${itemListTmp[@]}
@@ -95,6 +104,10 @@ func_relative_by_svn_append()
 			itemList2Tmp=(`svn status | grep -e '^?' -e '^!' | grep -ir $nextRelativeItem | grep -ir $secondCondition`)
 		elif [ "$modestatus" == 'commit' ]; then
 			itemList2Tmp=(`svn status | grep -e '^A' -e '^D' | grep -ir $nextRelativeItem | grep -ir $secondCondition`)
+		elif [ "$modestatus" == 'uncache' ]; then
+			itemList2Tmp=(`cat $uncachefile | grep -ir $nextRelativeItem | grep -ir $secondCondition`)
+		elif [ "$modestatus" == 'cache' ]; then
+			itemList2Tmp=(`cat $cachefile | grep -ir $nextRelativeItem | grep -ir $secondCondition`)
 		fi
 
 		for i in ${itemList2Tmp[@]}
@@ -165,6 +178,8 @@ unset cmd3
 unset svnstatus
 unset item_unknow_array
 unset item_commit_array
+unset item_uncache_array
+unset item_cache_array
 
 # 倒退鍵
 # Ctrl + h
@@ -209,17 +224,9 @@ do
 	elif [ "$mode" == '2' ]; then
 		cmd="svn status | grep -e '^A' -e '^M' -e '^D'"
 	elif [ "$mode" == '3' ]; then
-		if [ -f "$uncachefile" ]; then
-			cmd="cat $uncachefile"
-		else
-			cmd="touch $uncachefile"
-		fi
+		cmd="cat $uncachefile"
 	elif [ "$mode" == '4' ]; then
-		if [ -f "$cachefile" ]; then
-			cmd="cat $cachefile"
-		else
-			cmd="touch $cachefile"
-		fi
+		cmd="cat $cachefile"
 	fi
 	eval $cmd
 
@@ -277,6 +284,32 @@ do
 		echo "準備送出的SVN，狀態為A與D的檔案有找到一筆: ${item_commit_array[0]} [.]"
 	fi
 
+	# 顯示Uncache項目
+	if [ "${#item_uncache_array[@]}" -gt 1 ]; then
+		echo "重覆的Uncache檔案數量: ${#item_uncache_array[@]} [*]"
+		number=1
+		for bbb in ${item_uncache_array[@]}
+		do
+			echo "$number. $bbb"
+			number=$((number + 1))
+		done
+	elif [ "${#item_uncache_array[@]}" -eq 1 ]; then 
+		echo "Uncache檔案有找到一筆: ${item_uncache_array[0]} [.]"
+	fi
+
+	# 顯示Cache項目
+	if [ "${#item_cache_array[@]}" -gt 1 ]; then
+		echo "重覆的Cache檔案數量: ${#item_cache_array[@]} [*]"
+		number=1
+		for bbb in ${item_cache_array[@]}
+		do
+			echo "$number. $bbb"
+			number=$((number + 1))
+		done
+	elif [ "${#item_cache_array[@]}" -eq 1 ]; then 
+		echo "Cache檔案有找到一筆: ${item_cache_array[0]} [.]"
+	fi
+
 	# 不加IFS=012的話，我輸入空格，read variable是讀不到的
 	IFS=$'\012'
 	read -s -n 1 inputvar
@@ -292,6 +325,8 @@ do
 		unset svnstatus
 		unset item_unknow_array
 		unset item_commit_array
+		unset item_uncache_array
+		unset item_cache_array
 		continue
 	# 我也不知道，為什麼只能用Ctrl + H 來觸發倒退鍵的事件
 	elif [ "$inputvar" == $backspace ]; then
@@ -312,6 +347,45 @@ do
 		unset svnstatus
 		unset item_unknow_array
 		unset item_commit_array
+		unset item_uncache_array
+		unset item_cache_array
+		continue
+	elif [ "$inputvar" == 'C' ]; then
+		echo '要送出囉，但是請先輸入changelog，輸入完按Enter後直接送出'
+		read changelog
+		if [ "$changelog" == '' ]; then
+			echo '為什麼你沒有輸入changelog呢？還是我幫你填上預設值呢？(no comment)好嗎？[Y1,n0]'
+			read inputvar2
+			if [[ "$inputvar2" == 'y' || "$inputvar2" == "1" ]]; then
+				changelog='no comment'
+			elif [[ "$inputvar2" == 'n' || "$inputvar2" == "0" ]]; then
+				echo '如果不要預設值，那就算了'
+			else
+				echo '不好意思，不要預設值也不要來亂'
+			fi
+		fi
+		if [ "$changelog" == '' ]; then
+			echo '你並沒有輸入changelog，所以下次在見了，本次動作取消，倒數3秒後離開'
+			sleep 3
+		else
+			git commit -m "$changelog"
+			changelog=''
+			if [ "$?" -eq 0 ]; then
+				echo '送出成功'
+			else
+				echo '送出失敗，請自行做檢查'
+			fi
+			echo '按任何鍵繼續...'
+			read -n 1
+		fi
+
+		unset cmd
+		unset condition
+		unset svnstatus
+		unset item_unknow_array
+		unset item_commit_array
+		unset item_uncache_array
+		unset item_cache_array
 		continue
 	elif [ "$inputvar" == 'U' ]; then
 		svn update
@@ -326,6 +400,8 @@ do
 		unset svnstatus
 		unset item_unknow_array
 		unset item_commit_array
+		unset item_uncache_array
+		unset item_cache_array
 		continue
 	elif [ "$inputvar" == '.' ]; then
 		if [ ${#item_unknow_array[@]} -eq 1 ]; then
@@ -342,11 +418,44 @@ do
 				svn rm ${match:2}
 			fi
 
+			#cmd="svn status | grep -e '^A' -e '^D' -e '^M' > $uncachefile"
+			#eval $cmd
+
+			item_uncache_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "uncache" "$uncachefile" "$cachefile"` )
+
+			IFS=$default_ifs
+			num=0
+			for i in ${item_uncache_array[@]}
+			do
+				# 解決狀態與物件間的7個空白
+				# XXXXXXXXXXXXXXXXXXXXXXXX1234567X
+				handle1=`echo $i | sed 's/       /___/'`
+				handle2=`echo $handle1 | sed 's/ /___/g'`
+				# 為了要解決空白檔名的問題
+				item_uncache_array2[$num]=$handle2
+				num=$num+1
+			done
+			IFS=$default_ifs
+			num=0
+
+			cmd="rm $uncachefile"
+			eval $cmd
+			for bbb in ${item_uncache_array2[@]}
+			do
+				cmd="echo $bbb >> $uncachefile"
+				eval $cmd
+			done
+
+			rm $cachefile
+			touch $cachefile
+
 			unset cmd
 			unset condition
 			unset svnstatus
 			unset item_unknow_array
 			unset item_commit_array
+			unset item_uncache_array
+			unset item_cache_array
 			continue
 		elif [ ${#item_commit_array[@]} -eq 1 ]; then
 			# 不分兩次做，會出現前面少了一個空白，不知道為什麼
@@ -362,11 +471,73 @@ do
 				svn revert ${match:2}
 			fi
 
+
+			#cmd="svn status | grep -e '^A' -e '^D' -e '^M' > $uncachefile"
+			#eval $cmd
+
+			item_uncache_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "uncache" "$uncachefile" "$cachefile"` )
+
+			IFS=$default_ifs
+			num=0
+			for i in ${item_uncache_array[@]}
+			do
+				# 解決狀態與物件間的7個空白
+				# XXXXXXXXXXXXXXXXXXXXXXXX1234567X
+				handle1=`echo $i | sed 's/       /___/'`
+				handle2=`echo $handle1 | sed 's/ /___/g'`
+				# 為了要解決空白檔名的問題
+				item_uncache_array2[$num]=$handle2
+				num=$num+1
+			done
+			IFS=$default_ifs
+			num=0
+
+			cmd="rm $uncachefile"
+			eval $cmd
+			for bbb in ${item_uncache_array2[@]}
+			do
+				cmd="echo $bbb >> $uncachefile"
+				eval $cmd
+			done
+
+			rm $cachefile
+			touch $cachefile
+
 			unset cmd
 			unset condition
 			unset svnstatus
 			unset item_unknow_array
 			unset item_commit_array
+			unset item_uncache_array
+			unset item_cache_array
+			continue
+		elif [ ${#item_uncache_array[@]} -eq 1 ]; then
+			# 不分兩次做，會出現前面少了一個空白，不知道為什麼
+			#match=`echo ${item_uncache_array[0]} | sed 's/___/X/'`
+			#match=`echo $match | sed 's/___/ /g'`
+
+			cmd="grep \"${item_uncache_array[0]}\" $cachefile"
+			tmp1=`eval $cmd`
+			if [ "$tmp1" == '' ]; then
+				cmd="echo ${item_uncache_array[0]} >> $cachefile"
+				eval $cmd
+			fi
+
+			# 先寫到暫存，然後在回寫回原來的檔案
+			cmd="sed \"/${item_uncache_array[0]}/d\" $uncachefile > ${uncachefile}-tmp"
+			eval $cmd
+			cmd="cp ${uncachefile}-tmp $uncachefile"
+			eval $cmd
+			cmd="rm ${uncachefile}-tmp"
+			eval $cmd
+
+			unset cmd
+			unset condition
+			unset svnstatus
+			unset item_unknow_array
+			unset item_commit_array
+			unset item_uncache_array
+			unset item_cache_array
 			continue
 		fi
 	fi
@@ -381,9 +552,13 @@ do
 		cmd3=${cmds[2]}
 
 		if [ "$mode" == '1' ]; then
-			item_unknow_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "unknow"` )
+			item_unknow_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "unknow" "" ""` )
 		elif [ "$mode" == '2' ]; then
-			item_commit_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "commit"` )
+			item_commit_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "commit" "" ""` )
+		elif [ "$mode" == '3' ]; then
+			item_uncache_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "uncache" "$uncachefile" "$cachefile"` )
+		elif [ "$mode" == '4' ]; then
+			item_cache_array=( `func_relative_by_svn_append "$cmd1" "$cmd2" "$cmd3" "cache" "$uncachefile" "$cachefile"` )
 		fi
 	elif [ "$condition" == '' ]; then
 		# 會符合這裡的條件，是使用Ctrl + H 倒退鍵，把字元都砍光了以後會發生的狀況
@@ -392,6 +567,8 @@ do
 		unset svnstatus
 		unset item_unknow_array
 		unset item_commit_array
+		unset item_uncache_array
+		unset item_cache_array
 	fi
 
 done
