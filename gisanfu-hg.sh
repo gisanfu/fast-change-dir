3213eee63ecf81f98524cb24f0e9ef7f8cccf6d4
Jerry <gisanfu@gmail.com>
2011-01-04 15:07:27 +0800

    tune, 把abc-v3的概念帶過來

--- gisanfu-hg.sh
+++ gisanfu-hg.sh
@@ -243,16 +243,16 @@ func_relative_by_hg_append()
 	fi
 }
 
-unset condition
 unset cmd
 unset cmd1
 unset cmd2
 unset cmd3
-unset hgstatus
-unset item_unknow_array
-unset item_commit_array
-unset item_uncache_array
-unset item_cache_array
+
+# 只有第一次是1，有些只會執行一次，例如help
+first='1'
+
+# 當符合某些條件以後，所以動作都要重來，這時需要清除掉某一些變數的內容
+clear_var_all=''
 
 # 倒退鍵
 # Ctrl + h
@@ -274,6 +274,18 @@ func_cache_controller "$uncachefile" "$cachefile" "clear-uncache"
 while [ 1 ];
 do
 	clear
+
+	if [[ "$first" == '1' || "$clear_var_all" == '1' ]]; then
+		unset cmd
+		unset condition
+		unset hgstatus
+		unset item_unknow_array
+		unset item_commit_array
+		unset item_uncache_array
+		unset item_cache_array
+		clear_var_all=''
+		first='1'
+	fi
 
 	echo 'Mercurial(Hg) (關鍵字)'
 	echo '================================================='
@@ -400,13 +412,7 @@ do
 		clear
 		break
 	elif [ "$inputvar" == '/' ]; then
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	# 我也不知道，為什麼只能用Ctrl + H 來觸發倒退鍵的事件
 	elif [ "$inputvar" == $backspace ]; then
@@ -421,13 +427,7 @@ do
 			mode='1'
 		fi
 
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	elif [ "$inputvar" == 'B' ]; then
 		if [ "$mode" == '3' ]; then
@@ -438,13 +438,7 @@ do
 			mode='3'
 		fi
 
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	elif [ "$inputvar" == 'C' ]; then
 		if [[ "$mode" == '2' || "$mode" == '4' ]]; then
@@ -526,13 +520,7 @@ do
 			fi
 		fi
 
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	elif [ "$inputvar" == 'D' ]; then
 		if [ "$mode" == '3' ]; then
@@ -541,13 +529,7 @@ do
 			func_cache_controller "$uncachefile" "$cachefile" "clear-cache"
 		fi
 
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	elif [ "$inputvar" == 'E' ]; then
 		hg push
@@ -557,26 +539,14 @@ do
 		echo '按任何鍵繼續...'
 		read -n 1
 
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	elif [ "$inputvar" == 'G' ]; then
 		# 匯出後，自動跳到模式3，就是Uncache mode
 		func_cache_controller "$uncachefile" "$cachefile" "hg-status-to-uncache"
 		mode='3'
 
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	elif [ "$inputvar" == 'U' ]; then
 		hg pull
@@ -589,13 +559,7 @@ do
 		echo '按任何鍵繼續...'
 		read -n 1
 
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
 		continue
 	elif [ "$inputvar" == '.' ]; then
 		if [ ${#item_unknow_array[@]} -eq 1 ]; then
@@ -614,13 +578,7 @@ do
 
 			func_cache_controller "$uncachefile" "$cachefile" "hg-status-to-uncache"
 
-			unset cmd
-			unset condition
-			unset hgstatus
-			unset item_unknow_array
-			unset item_commit_array
-			unset item_uncache_array
-			unset item_cache_array
+			clear_var_all='1'
 			continue
 		elif [ ${#item_commit_array[@]} -eq 1 ]; then
 			# 不分兩次做，會出現前面少了一個空白，不知道為什麼
@@ -638,13 +596,7 @@ do
 
 			func_cache_controller "$uncachefile" "$cachefile" "hg-status-to-uncache"
 
-			unset cmd
-			unset condition
-			unset hgstatus
-			unset item_unknow_array
-			unset item_commit_array
-			unset item_uncache_array
-			unset item_cache_array
+			clear_var_all='1'
 			continue
 		elif [ ${#item_uncache_array[@]} -eq 1 ]; then
 			cmd="grep '${item_uncache_array[0]}' $cachefile"
@@ -663,13 +615,7 @@ do
 			cmd="rm -rf ${uncachefile}-tmp"
 			eval $cmd
 
-			unset cmd
-			unset condition
-			unset hgstatus
-			unset item_unknow_array
-			unset item_commit_array
-			unset item_uncache_array
-			unset item_cache_array
+			clear_var_all='1'
 			continue
 		elif [ ${#item_cache_array[@]} -eq 1 ]; then
 			cmd="grep '${item_cache_array[0]}' $uncachefile"
@@ -688,13 +634,7 @@ do
 			cmd="rm -rf ${cachefile}-tmp"
 			eval $cmd
 
-			unset cmd
-			unset condition
-			unset hgstatus
-			unset item_unknow_array
-			unset item_commit_array
-			unset item_uncache_array
-			unset item_cache_array
+			clear_var_all='1'
 			continue
 		fi
 	fi
@@ -719,13 +659,9 @@ do
 		fi
 	elif [ "$condition" == '' ]; then
 		# 會符合這裡的條件，是使用Ctrl + H 倒退鍵，把字元都砍光了以後會發生的狀況
-		unset cmd
-		unset condition
-		unset hgstatus
-		unset item_unknow_array
-		unset item_commit_array
-		unset item_uncache_array
-		unset item_cache_array
+		clear_var_all='1'
+
+		# 這裡不用在continue了，因為這裡是最後一行了
 	fi
 
 done
