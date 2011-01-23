b4a9c5da390c89fbd7d702b1c25d373b60bd9fc6
Jerry <gisanfu@gmail.com>
2011-01-23 18:37:16 +0800

    add check binary filetype function

---
+++ gisanfu-only-text-filecontent.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 本程式目的
+# 忽略掉純文字以外的檔案，例如壓縮檔
+
+# 如果你要使用這支程式來當做pipe使用
+# 請使用your-runfilename | xargs -n 1 this-file-name
+
+aaa=$1
+
+bbb=`file $aaa | grep text | wc -l`
+
+if [ "$bbb" -gt 0 ]; then
+	echo $aaa
+fi
--- gisanfu-vimlist.sh
+++ gisanfu-vimlist.sh
@@ -11,7 +11,19 @@ if [ "$groupname" != "" ]; then
 		program2=$program
 	fi
 
-	cmdlist=`cat ~/gisanfu-vimlist-$groupname.txt | tr "\n" " "`
+	cmdlist="cat ~/gisanfu-vimlist-$groupname.txt"
+
+	# 如果program這個變數是空白，就代表會使用vim-p的指令
+	# 使用vim，當然不會去開一些binary的檔案
+	if [ "$program" == '' ]; then
+		# 先把一些己知的東西先ignore掉，例如壓縮檔
+		cmdlist="$cmdlist | grep -v .tar.gz | grep -v .zip"
+		cmdlist="$cmdlist | xargs -n 1 $fast_change_dir/gisanfu-only-text-filecontent.sh"
+	fi
+
+	# 這是多行文字檔內容，變成以空格分格成字串的步驟
+	cmdlist2='| tr "\n" " "'
+	cmdlist="$cmdlist $cmdlist2"
 	cmd="$program2 $cmdlist"
 
 	# 檢查一下數量，如果是一筆的話，那就自動跳到那一筆
